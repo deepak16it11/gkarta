fe82a5e99c8bd0cc9afbb9e790bed610490d2335
deepak16it11 <47378404+deepak16it11@users.noreply.github.com>
2019-02-06 12:11:52 +0530

    Create deepkak01.java

---
+++ deepkak01.java
@@ -0,0 +1,18 @@
+public class PositiveNegative {
+
+    public static void main(String[] args) {
+      double number = 3;
+        if (number < 0)
+        {
+            System.out.println("  Negative ");
+        }
+        else if ( number > 0)
+        {
+            System.out.println( "  Positive ");
+        }
+        else
+        {
+            System.out.println(" Zero");
+        }
+    }
+}
