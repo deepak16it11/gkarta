a72c0a92000c66e919e403682b749ef21b5d61ba
deepak16it11 <47378404+deepak16it11@users.noreply.github.com>
2019-03-08 11:22:37 +0530

    Create bg08.java

---
+++ bg08.java
@@ -0,0 +1,14 @@
+import java.util.*;
+  class sumofN{
+
+     public static void main(String []args){
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+        int sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            sum=sum+i;
+        }
+        System.out.print(sum);
+     }
+}
